a6560d2f5e1623aea88d3d07c705125354c38545
Amrit Bharati <amritbharati15@gmail.com>
2025-09-12 11:01:36 +0545

    Add files via upload

---
+++ Lecture4/Ex1.py
@@ -0,0 +1,11 @@
+# average of 3 numbers
+
+def calc_avg(a,b,c):
+    sum= a+b+c 
+    average= sum/3
+    return average
+print(calc_avg(9,2,4))
+
+print(calc_avg(81,39,45))
+
+
---
+++ Lecture4/Function.py
@@ -0,0 +1,27 @@
+# Functions in Python
+#  ( Block of statements that perform a specific task.)
+
+
+
+'''
+def func_name(param1, param2...):
+    # some work
+    return value 
+func_name(arg1, arg2...)  # function call
+
+
+'''
+        # we should use function to remove the redundancy 
+                           
+def calc_sum(a,b):
+    sum= a+b
+    return sum
+print(calc_sum(5,9))
+
+
+def Calc_sum(a,b):
+    return a+b
+sum= Calc_sum(1,2)
+print(sum)
+
+
