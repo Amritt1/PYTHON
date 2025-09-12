e8f548c9299a6ef6b3bb7deb065cc4503ee56f2f
Amrit Bharati <amritbharati15@gmail.com>
2025-09-12 10:55:18 +0545

    Delete Practicequ3(13).py

--- Practicequ3(13).py
+++
@@ -1,8 +0,0 @@
-
-# WAP to input floating point numbers & print their average.
-
-num1 = float(input("Enter number1:"))
-num2 = float(input("Enter number2:"))
-
-avg= (num1+num2)/2
-print("Average",avg)
